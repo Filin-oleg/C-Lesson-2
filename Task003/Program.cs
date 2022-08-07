cdac3c28f2128030621ea0a523c319f31d09b161
Filin-oleg <110037084+Filin-oleg@users.noreply.github.com>
2022-08-07 11:39:35 +0200

    done

---
+++ Task003/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {11, 21, 31, 41, 18, 51, 15, 17, 18, 19};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
